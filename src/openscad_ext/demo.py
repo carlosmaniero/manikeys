e04ede5f82a6f1b5258d8e1cce3a1f00315c83d5
Carlos Maniero <carlos@maniero.me>
2026-03-02 22:26:48 -0300

    add an slicer like mechanis no built a shape from many 2d layers
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

---
+++ src/openscad_ext/demo.py
@@ -0,0 +1,21 @@
+from .slicer import slicer, Slice
+import math
+
+
+def sphere_slices(z: float) -> Slice:
+    radius = 25
+    r_sq = radius**2 - (z - radius) ** 2
+    r = math.sqrt(max(0.0, r_sq))
+
+    r = max(r, 0.01)
+
+    return Slice(
+        upper=lambda x: math.sqrt(max(0.0, r**2 - x**2)),
+        lower=lambda x: -math.sqrt(max(0.0, r**2 - x**2)),
+        x_range=(-r, r),
+        slices=100,
+    )
+
+
+def show_sphere():
+    slicer(sphere_slices, height=50, fn=100).show()
