83d5e79b4f193c01bca05e6076afaec932bcaa8b
Carlos Maniero <carlos@maniero.me>
2026-03-05 00:17:23 -0300

    create the structure to detect it is the thumb section
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

--- src/cad/body.py
+++ src/cad/body.py
@@ -27,7 +27,7 @@ class Body:
     def assembly(self):
         return loft(
             lambda x: Profile(
-                upper=lambda y: self._profile_callback(x, y),
+                upper=lambda y: self._get_top_z(x, y),
                 lower=lambda y: -self.parameters.body.height,
                 span=(
                     self._get_start_y(x),
@@ -36,14 +36,28 @@ class Body:
                 segments=200,
             ),
             span=(self._get_start_x(), self._get_end_x()),
-            fn=20,
+            fn=200,
         ).rotate([90, 0, 90])
+
+    def _highest_cap(self) -> float:
+        return (
+            self.layout.positioning.highest[2] + self.parameters.caps.size / 2
+        )
 
     def _thumb_section_width(self) -> float:
         return (self.parameters.caps.size + self.parameters.caps.gap) * 3
 
     def _thumb_section_height(self) -> float:
         return (self.parameters.caps.size + self.parameters.caps.gap) * 2
+
+    def _is_thumb_section(self, x: float, y: float) -> bool:
+        return self._is_thumb_section_x(x) and self._is_thumb_section_y(y)
+
+    def _is_thumb_section_x(self, x: float) -> bool:
+        return x >= self._get_end_x() - self._thumb_section_width()
+
+    def _is_thumb_section_y(self, y: float) -> bool:
+        return y < self._caps_start_y()
 
     def _caps_start_y(self) -> float:
         return (
@@ -55,7 +69,7 @@ class Body:
     def _get_start_y(self, x: float) -> float:
         delta = 0
 
-        if x >= self._get_end_x() - self._thumb_section_width():
+        if self._is_thumb_section_x(x):
             delta = self._thumb_section_width()
 
         return self._caps_start_y() - delta
@@ -76,8 +90,26 @@ class Body:
     def _get_end_x(self) -> float:
         return 180 + self._get_start_x()
 
-    def _profile_callback(self, x: float, y: float) -> float:
-        return 0
+    def _get_top_z(self, x: float, y: float) -> float:
+        if self._is_thumb_section(x, y):
+            return 10
+
+        sphere_z = self._get_sphere_lower_z(x, y)
+
+        if sphere_z > self._highest_cap():
+            return self._highest_cap()
+
+        return sphere_z
+
+    def _get_sphere_lower_z(self, x, y):
+        radius = self.parameters.body.radius
+        distance = math.sqrt(x**2 + y**2)
+
+        if distance >= radius:
+            return radius
+        else:
+            result = radius - math.sqrt(radius**2 - distance**2)
+            return result
 
 
 if __name__ == "__main__":
