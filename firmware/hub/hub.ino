c14e9183a1794a8788e6fcf17614c44ee246ff32
Carlos Maniero <carlos@maniero.me>
2026-07-11 17:34:17 -0300

    add the hub (master) firmware
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

---
+++ firmware/hub/hub.ino
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+#include <comm_spi.h>
+#include <msgs.h>
+
+const uint8_t SLAVE_PIN = 9;
+
+void setup() {
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; 
+  }
+  
+  Serial.println("Hub initialized. Setting up SPI...");
+  
+  comm_spi_set_master();
+  comm_spi_add_slave(SLAVE_PIN);
+}
+
+void loop() {
+  comm_spi_start_transaction(SLAVE_PIN);
+  
+  uint8_t response = comm_spi_transfer(MSG_NULL_BYTE);
+  
+  comm_spi_end_transaction(SLAVE_PIN);
+  
+  if (response == MSG_HEARTBEAT_BYTE) {
+    Serial.println("Slave on pin 9 is ALIVE! (Received Heartbeat)");
+  } else {
+    Serial.print("Slave is dead or not ready. Received: 0x");
+    Serial.println(response, HEX);
+  }
+  
+  delay(10);
+}
