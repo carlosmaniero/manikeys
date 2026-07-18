8243ef5d5e07014cc95a0426b48f04ffd77431ff
Carlos Maniero <carlosmaniero@gmail.com>
2026-07-18 20:35:51 -0300

    use only the perimeter to optimize the quantity of points
    Signed-off-by: Carlos Maniero <carlosmaniero@gmail.com>

--- src/core/pyvista_ext.py
+++ src/core/pyvista_ext.py
@@ -15,15 +15,31 @@ class VistaObject(Object[pv.PolyData]):
         obj.plot()
 
 
-def normalize_to_grid(
+def normalize_to_surface(
     x: npt.NDArray[np.float64],
     y: npt.NDArray[np.float64],
     z: npt.NDArray[np.float64] | float,
-) -> pv.StructuredGrid:
+) -> pv.PolyData:
     if isinstance(z, (int, float)):
-        bz = np.full_like(x, z)
-        return pv.StructuredGrid(x, y, bz)
-    return pv.StructuredGrid(x, y, z)
+        nx, ny = x.shape
+        perimeter_x = []
+        perimeter_y = []
+        for j in range(ny):
+            perimeter_x.append(x[0, j])
+            perimeter_y.append(y[0, j])
+        for i in range(1, nx):
+            perimeter_x.append(x[i, ny - 1])
+            perimeter_y.append(y[i, ny - 1])
+        for j in range(ny - 2, -1, -1):
+            perimeter_x.append(x[nx - 1, j])
+            perimeter_y.append(y[nx - 1, j])
+        for i in range(nx - 2, 0, -1):
+            perimeter_x.append(x[i, 0])
+            perimeter_y.append(y[i, 0])
+        pts = np.column_stack((perimeter_x, perimeter_y, np.full_like(perimeter_x, z)))
+        faces = [len(pts)] + list(range(len(pts)))
+        return pv.PolyData(pts, faces).triangulate()
+    return pv.StructuredGrid(x, y, z).extract_surface(algorithm=None)
 
 
 def create_full_surface(
@@ -32,8 +48,6 @@ def create_full_surface(
     top_z: npt.NDArray[np.float64] | float,
     bottom_z: npt.NDArray[np.float64] | float,
 ) -> pv.PolyData:
-    top_surface = normalize_to_grid(x, y, top_z).extract_surface(algorithm=None)
-    bottom_surface = normalize_to_grid(x, y, bottom_z).extract_surface(
-        algorithm=None
-    )
+    top_surface = normalize_to_surface(x, y, top_z)
+    bottom_surface = normalize_to_surface(x, y, bottom_z)
     return top_surface + bottom_surface
