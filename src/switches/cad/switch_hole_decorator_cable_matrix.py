9755c3d2c6f4be5c063b8ae03a7a8586a3874c9b
Carlos Maniero <carlos@maniero.me>
2026-09-15 15:58:32 -0300

    extracts the matrix organizer to a file
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

---
+++ src/switches/cad/switch_hole_decorator_cable_matrix.py
@@ -0,0 +1,72 @@
+from __future__ import annotations
+import sys
+from dataclasses import dataclass
+import manifold3d
+from injector import inject, singleton
+from switches.model import SwitchHoleDecoratorShellModel
+from core.manifold_ext.helpers import half_rounded
+from core.manifold_ext.object import ManifoldObject
+from core.context import injector
+
+
+@singleton
+@inject
+@dataclass
+class SwitchHoleDecoratorCableMatrixCAD(ManifoldObject):
+    model: SwitchHoleDecoratorShellModel
+
+    @property
+    def y_cable_path(self) -> manifold3d.Manifold:
+        body = half_rounded(
+            self.model.y_cable_path_cube_size,
+        ).rotate([180, 0, 0])
+        return body.translate(self.model.y_cable_path_translation)
+
+    @property
+    def x_cable_path(self) -> manifold3d.Manifold:
+        body = (
+            half_rounded(
+                self.model.x_cable_path_cube_size,
+            )
+            .rotate([0, 0, 90])
+            .rotate([180, 0, 0])
+        )
+        return body.translate(self.model.x_cable_path_translation)
+
+    @property
+    def cable_hole(self) -> manifold3d.Manifold:
+        hole = manifold3d.Manifold.cylinder(
+            self.model.cable_hole_length,
+            self.model.cable_hole_radius,
+            center=True,
+            circular_segments=32,
+        )
+        return hole.rotate([90, 0, 0]).translate(
+            self.model.y_cable_path_translation
+        )
+
+    @property
+    def x_cable_hole(self) -> manifold3d.Manifold:
+        hole = manifold3d.Manifold.cylinder(
+            self.model.x_cable_hole_length,
+            self.model.cable_hole_radius,
+            center=True,
+            circular_segments=32,
+        )
+        return hole.rotate([0, 90, 0]).translate(
+            self.model.x_cable_hole_translation
+        )
+
+    def assemble(self) -> manifold3d.Manifold:
+        return (
+            manifold3d.Manifold.hull(self.y_cable_path + self.x_cable_path)
+            - self.cable_hole
+            - self.x_cable_hole
+        )
+
+
+if __name__ == "__main__":
+    switch_hole_decorator_cable_matrix = injector.get(
+        SwitchHoleDecoratorCableMatrixCAD
+    )
+    switch_hole_decorator_cable_matrix.program(sys.argv)
