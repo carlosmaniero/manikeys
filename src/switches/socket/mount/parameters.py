4404002a6ffceac9510177b30df2d385c3a146d9
Carlos Maniero <carlos@maniero.me>
2026-09-21 17:00:55 -0300

    make the pcb placement structure stronger
    Signed-off-by: Carlos Maniero <carlos@maniero.me>

--- src/switches/socket/mount/parameters.py
+++ src/switches/socket/mount/parameters.py
@@ -9,6 +9,6 @@ class MountScrewCylinderParameters:
 @dataclass
 class PcbsPlacementParameters:
     clearance: float = 0.2
-    thickness: float = 1.6
+    thickness: float = 2
     wall_margin: float = 5.0
     arduinos_offset_z: float = -15.0
